16e5947579b50864bbbac29893a97f57ec76742b
fhemery <f.hemery@hemit.fr>
2023-08-21 14:54:40 +0200

    [php] Evenchecker solution

--- php/tests/evenChecker/EvenCheckerTest.php
+++ php/tests/evenChecker/EvenCheckerTest.php
@@ -2,7 +2,7 @@
 
 namespace EvenChecker;
 
-// use CodeKatas\EvenChecker;
+use CodeKatas\EvenChecker;
 use PHPUnit\Framework\TestCase;
 
 class EvenCheckerTest extends TestCase
@@ -11,5 +11,21 @@ class EvenCheckerTest extends TestCase
     public function testShouldWork(): void
     {
         $this->assertEquals(3, 1 + 2);
+    }
+
+    private $checker;
+
+    public function setup(): void {
+        $this->checker = new EvenChecker();
+    }
+
+    public function testShouldReturnTrueFor2() {
+        $this->checker->add(2);
+        $this->assertTrue($this->checker->isEven());
+    }
+
+    public function testShouldReturnFalseFor7() {
+        $this->checker->add(7);
+        $this->assertFalse($this->checker->isEven());
     }
 }
