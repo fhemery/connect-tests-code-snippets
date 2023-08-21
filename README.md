4b384da90843a859cc5c9c0d6a958efa9a6fb203
fhemery <f.hemery@hemit.fr>
2023-08-21 14:52:13 +0200

    [php] Evenchecker code

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 This repository contains the code snippets used for Connect Test module. In particular :
 
+- [Even checker kata](./php/src/EvenChecker/README.md)
 - [Tennis score base kata](./ts/src/tennis-kata/README.md)
---
+++ php/tests/evenChecker/EvenCheckerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EvenChecker;
+
+// use CodeKatas\EvenChecker;
+use PHPUnit\Framework\TestCase;
+
+class EvenCheckerTest extends TestCase
+{
+    // Write tests here
+    public function testShouldWork(): void
+    {
+        $this->assertEquals(3, 1 + 2);
+    }
+}
