d28576e1c05f5d3ec6835945416b2973a1b72171
fhemery <f.hemery@hemit.fr>
2023-08-23 15:01:55 +0200

    Add solution for TripService

--- ts/src/trip-service/trip-service.spec.ts
+++ ts/src/trip-service/trip-service.spec.ts
@@ -1,5 +1,71 @@
+import TripService from "./trip/TripService";
+import User from "./user/User";
+import UserNotLoggedInException from "./exception/UserNotLoggedInException";
+import UserSession from "./user/UserSession";
+import Trip from "./trip/Trip";
+
 describe("Trip Service", function () {
-  it("should work", () => {
-    expect(true).toBe(true);
+  let service: TripService;
+
+  beforeEach(() => {
+    service = new TripService();
+  });
+
+  it("should throw an exception if currentUser is null (with Jest)", function () {
+    UserSession.getLoggedUser = jest.fn().mockReturnValue(null);
+    expect(() => service.getTripsByUser(new User(), null)).toThrowError(
+      new UserNotLoggedInException()
+    );
+  });
+
+  class TripServiceOverload extends TripService {
+    trips: Trip[] = [];
+
+    protected getTrips(): Trip[] {
+      return this.trips;
+    }
+
+    setTrips(trips: Trip[]) {
+      this.trips = trips;
+    }
+  }
+
+  it("should throw an exception is currentUser is null (with overload)", function () {
+    const overload = new TripServiceOverload();
+    expect(() => overload.getTripsByUser(new User(), null)).toThrowError(
+      new UserNotLoggedInException()
+    );
+  });
+
+  it("should return no trip when current user is not null and user has no friend", function () {
+    const overload = new TripServiceOverload();
+    expect(overload.getTripsByUser(new User(), new User())).toEqual([]);
+  });
+
+  it("should return no trip when current user is not null and user is not friend with current user", function () {
+    const overload = new TripServiceOverload();
+    const alice = new User();
+
+    const bob = new User();
+    const carol = new User();
+
+    bob.addFriend(carol);
+    const toRome = new Trip();
+    overload.setTrips([toRome]);
+    expect(overload.getTripsByUser(bob, alice)).toEqual([]);
+  });
+
+  it("should return trips when current user is not null and user is friend with current user", function () {
+    const overload = new TripServiceOverload();
+    const alice = new User();
+
+    const bob = new User();
+    const carol = new User();
+
+    bob.addFriend(carol);
+    bob.addFriend(alice);
+    const toRome = new Trip();
+    overload.setTrips([toRome]);
+    expect(overload.getTripsByUser(bob, alice)).toEqual([toRome]);
   });
 });
--- ts/src/trip-service/trip/TripService.ts
+++ ts/src/trip-service/trip/TripService.ts
@@ -1,30 +1,21 @@
 import UserNotLoggedInException from "../exception/UserNotLoggedInException";
 import User from "../user/User";
-import UserSession from "../user/UserSession";
 import Trip from "./Trip";
 import TripDAO from "./TripDAO";
 
 export default class TripService {
-    public getTripsByUser(user: User): Trip[] {
-        let tripList: Trip[] = [];
-        const loggedUser: User = UserSession.getLoggedUser();
-        let isFriend: boolean = false;
-
-        if (loggedUser != null) {
-            for (const friend of user.getFriends()) {
-                if (friend === loggedUser) {
-                    isFriend = true;
-                    break;
-                }
-            }
-
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user);
-            }
-
-            return tripList;
-        } else {
-            throw new UserNotLoggedInException();
-        }
+  public getTripsByUser(user: User, loggedUser: User | null): Trip[] {
+    if (loggedUser == null) {
+      throw new UserNotLoggedInException();
     }
+
+    if (user.getFriends().includes(loggedUser)) {
+      return this.getTrips(user);
+    }
+    return [];
+  }
+
+  protected getTrips(user: User) {
+    return TripDAO.findTripsByUser(user);
+  }
 }
